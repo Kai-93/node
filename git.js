477e122df02de6837769294cbaa247641903ff4a
Kaiser <765934690@qq.com>
2017-05-18 14:58:59 +0800

    tt

--- git.js
+++ git.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 
 app.post('/push_www', function (req, res) {
   //exec sh
-  process.exec('/home/ubuntu/node/git-node.sh', null, function (err, stdout, stderr) {
+  process.exec('/home/ubuntu/node/www.sh', null, function (err, stdout, stderr) {
     console.log(err)
     console.log(stdout)
     console.log(stderr)
@@ -27,7 +27,7 @@ app.post('/push_www', function (req, res) {
 
 app.post('/push_node', function (req, res) {
   //exec sh
-  process.exec('/home/ubuntu/node/git-node.sh', null, function (err, stdout, stderr) {
+  process.exec('/home/ubuntu/node/node.sh', null, function (err, stdout, stderr) {
     console.log(err)
     console.log(stdout)
     console.log(stderr)
