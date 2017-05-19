7649a050b55bb0512e89d35bf31e891b8320ba4d
Kaiser <765934690@qq.com>
2017-05-19 14:36:20 +0800

    tt

--- git.js
+++ git.js
@@ -5,28 +5,9 @@
 const express = require('express')
 const http = require('http')
 const app = express()
-const process = require('child_process')
 
 app.get('/', function (req, res) {
   res.sendFile('/home/ubuntu/www')
-})
-
-app.post('/push_www', function (req, res) {
-  process.exec('/home/ubuntu/node/www.sh', null, function (err, stdout, stderr) {
-    console.log(err)
-    console.log(stdout)
-    console.log(stderr)
-    res.send('ok')
-  })
-})
-
-app.post('/push_node', function (req, res) {
-  process.exec('/home/ubuntu/node/node.sh', null, function (err, stdout, stderr) {
-    console.log(err)
-    console.log(stdout)
-    console.log(stderr)
-    res.send('ok')
-  })
 })
 
 let httpServer = http.createServer(app)
